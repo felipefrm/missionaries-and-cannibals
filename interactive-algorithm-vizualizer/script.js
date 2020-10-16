cc95b318bf02e08dc16f961440e8f82edfee981f
heitor57 <heitorwerneck@hotmail.com>
2020-10-15 23:25:51 -0300

    working

--- interactive-algorithm-vizualizer/script.js
+++ interactive-algorithm-vizualizer/script.js
@@ -1,23 +1,4 @@
-
-var state = {
-    left: {
-        missionaries: 3,
-        canibals: 3,
-        boat: true,
-    },
-    right: {
-        missionaries: 0,
-        canibals: 0,
-        boat: false
-    }
-}
-
-var boat = {
-    crew: [],
-    count: 0,
-};
-
-async function updateState() {
+async function updateState(state) {
 
     const left = $('.margem-esquerda .row')
     left.empty()
@@ -36,17 +17,10 @@ async function updateState() {
     for (let i=0; i<state['right'].canibals; i++) {
         right.append('<div class="col-sm-1 canibal"></div>')
     }
-
-    $('input[name="leftMissionaries"]').val(state['left'].missionaries);
-    $('input[name="leftCanibals"]').val(state['left'].canibals);
-    $('input[name="rightMissionaries"]').val(state['right'].missionaries);
-    $('input[name="rightCanibals"]').val(state['right'].canibals);
-
-    updateButtonsState()
 }
 
 
-function updateBoat() {
+function updateBoat(boat) {
     const ship = $('.tripulantes')
     ship.empty();
     for (let i = 0; i < boat.count; i++) {
@@ -55,105 +29,91 @@ function updateBoat() {
 
 }
 
-function updateButtonsState() {
-    const boatPosition = getBoatPosition()
-    if (state[boatPosition].missionaries === 0 || boat.count === 2) {
-        $('#missionarieToBoat').attr('disabled','disabled');
-    }
-    else {
-        $('#missionarieToBoat').removeAttr('disabled');
-    }
-
-    if (state[boatPosition]. canibals === 0  || boat.count === 2) {
-        $('#canibalToBoat').attr('disabled','disabled');
-    }
-    else {
-        $('#canibalToBoat').removeAttr('disabled');
-    }
-
-    if (boat.crew[0] !== 'missionario' && boat.crew[1] !== 'missionario') {
-        $('#missionarieFromBoat').attr('disabled', 'disabled');
-    }
-    else {
-        $('#missionarieFromBoat').removeAttr('disabled');
-    }
-
-    if (boat.crew[0] !== 'canibal' && boat.crew[1] !== 'canibal') {
-        $('#canibalFromBoat').attr('disabled', 'disabled');
-    }
-    else {
-        $('#canibalFromBoat').removeAttr('disabled');
-    }
-
-    if (boat.count === 0) {
-        $('#boat').attr('disabled','disabled');
-    }
-    else {
-        $('#boat').removeAttr('disabled');
-    }
-}
-
-async function previousEvent() {
-
-}
-
 async function nextEvent(currentStateType,currentState, parentStateType,parentState) {
 
-    if(currentStateType!=='Valid' || parentStateType!=='Valid')
-        console.log("invalid state")
+    var state = {
+        left: {
+            missionaries: 3,
+            canibals: 3,
+            boat: true,
+        },
+        right: {
+            missionaries: 0,
+            canibals: 0,
+            boat: false
+        }
+    }
+
+    var boat = {
+        crew: [],
+        count: 0,
+    };
+
+    if(currentStateType!=='valid' || parentStateType!=='valid'){
         return
+    }
+
+    $('.tripulantes').empty()
+    parentState = parentState.replace("(", "");
+    parentState = parentState.replace(")", "");
     parentState = parentState.replace("(", "");
     parentState = parentState.replace(")", "");
     let arrayParentState = parentState.split(",");
 
-    state.left.missionaries = parseInt(arrayParentState[1]);
-    state.left.canibals = parseInt(arrayParentState[2]);
-    state.left.boat = parseInt(arrayParentState[3]);
-    state.right.missionaries = parseInt(arrayParentState[4]);
-    state.right.canibals = parseInt(arrayParentState[5]);
-    state.right.boat = parseInt(arrayParentState[6]);
+    state.left.missionaries = parseInt(arrayParentState[0]);
+    state.left.canibals = parseInt(arrayParentState[1]);
+    state.left.boat = parseInt(arrayParentState[2]);
+    state.right.missionaries = parseInt(arrayParentState[3]);
+    state.right.canibals = parseInt(arrayParentState[4]);
+    state.right.boat = parseInt(arrayParentState[5]);
+    updateState(state)
 
-    updateState()
-    
+    if(state.right.boat){
+        $('.barco').css({transform: 'translateX(210px)'});
+    }else{
+        $('.barco').css({transform: 'translateX(0px)'});
+    }
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    await sleep(10);
+    currentState = currentState.replace("(", "");
+    currentState = currentState.replace(")", "");
     currentState = currentState.replace("(", "");
     currentState = currentState.replace(")", "");
     var arrayCurrentState = currentState.split(",");
 
-    if (arrayCurrentState[1] < 0 || arrayCurrentState[2] < 0 || arrayCurrentState[4] < 0 || arrayCurrentState[5] < 0) {
-        console.log('invalido');
-    }
-
-    diffMissionarie = Math.abs(arrayParentState[1] - arrayCurrentState[1])
-    diffCanibal = Math.abs(arrayParentState[2] - arrayCurrentState[2])
+    diffMissionarie = Math.abs(arrayParentState[0] - arrayCurrentState[0])
+    diffCanibal = Math.abs(arrayParentState[1] - arrayCurrentState[1])
 
     for(let i=0; i<diffMissionarie; i++) {
-      addMissionarieToBoat();
+        addMissionarieToBoat(state,boat);
     }
 
     for(let i=0; i<diffCanibal; i++) {
-      addCanibalToBoat();
+        addCanibalToBoat(state,boat);
     }
 
-    await moveBoat();
+    await moveBoat(state);
 
     for(let i=0; i<diffMissionarie; i++) {
-      removeMissionarieToBoat();
+        removeMissionarieToBoat(state,boat);
     }
 
     for(let i=0; i<diffCanibal; i++) {
-      removeCanibalToBoat();
+        removeCanibalToBoat(state,boat);
     }
 
 }
 
-function getBoatPosition() {
+function getBoatPosition(state) {
     if (state.right.boat) return 'right'
     else                  return 'left'
 }
 
-function addMissionarieToBoat() {
+function addMissionarieToBoat(state,boat) {
     const ship = $('.tripulantes')
-    const boatPosition = getBoatPosition()
+    const boatPosition = getBoatPosition(state)
     if (state[boatPosition].missionaries !== 0 && boat.count < 2) {
         ship.append('<div class="col-sm-1 missionario"></div>')
         boat.crew.push('missionario')
@@ -161,56 +121,56 @@ function addMissionarieToBoat() {
         console.log("antes do add ", boatPosition, state[boatPosition].missionaries)
         state[boatPosition].missionaries--;
         console.log("depois de add ", boatPosition, state[boatPosition].missionaries)
-        updateState()
-        updateBoat()
+        updateState(state)
+        updateBoat(boat)
     }
 }
 
-function addCanibalToBoat() {
+function addCanibalToBoat(state,boat) {
     const ship = $('.tripulantes')
-    const boatPosition = getBoatPosition()
+    const boatPosition = getBoatPosition(state)
     if (state[boatPosition].canibals !== 0 && boat.count < 2) {
         ship.append('<div class="col-sm-1 canibal"></div>')
         boat.crew.push('canibal')
         boat.count++
         state[boatPosition].canibals--;
-        updateState()
-        updateBoat()
+        updateState(state)
+        updateBoat(boat)
     }
 }
 
-function removeMissionarieToBoat() {
-    const boatPosition = getBoatPosition()
+function removeMissionarieToBoat(state,boat) {
+    const boatPosition = getBoatPosition(state)
     const missionarieIndex = boat.crew.findIndex(ind => ind === 'missionario');
     if (missionarieIndex !== -1) {
         boat.crew.splice(missionarieIndex, 1);
         boat.count--;
         state[boatPosition].missionaries++;
-        updateState();
-        updateBoat();
+        updateState(state);
+        updateBoat(boat);
     }
 }
 
-function removeCanibalToBoat() {
-    const boatPosition = getBoatPosition()
+function removeCanibalToBoat(state,boat) {
+    const boatPosition = getBoatPosition(state)
     const canibalIndex = boat.crew.findIndex(ind => ind === 'canibal');
     console.log(canibalIndex)
     if (canibalIndex !== -1) {
         boat.crew.splice(canibalIndex, 1);
         boat.count--;
         state[boatPosition].canibals++;
-        updateState();
-        updateBoat();
+        updateState(state);
+        updateBoat(boat);
     }
 }
 
 
-async function moveBoat() {
+async function moveBoat(state,duration=500) {
 
     console.log('inicio')
 
     move.defaults = {
-        duration: 1500
+        duration: duration
     }
 
     if (state.left.boat) {
@@ -223,20 +183,18 @@ async function moveBoat() {
         state.right.boat = false;
         state.left.boat = true;
     }
-    updateState(); 
+    updateState(state); 
     
 }
 
 
-updateState();
+// $('#missionarieToBoat').on('click', addMissionarieToBoat)
+// $('#canibalToBoat').on('click', addCanibalToBoat)    
 
-$('#missionarieToBoat').on('click', addMissionarieToBoat)
-$('#canibalToBoat').on('click', addCanibalToBoat)    
+// $('#missionarieFromBoat').on('click', removeMissionarieToBoat)
+// $('#canibalFromBoat').on('click', removeCanibalToBoat)   
 
-$('#missionarieFromBoat').on('click', removeMissionarieToBoat)
-$('#canibalFromBoat').on('click', removeCanibalToBoat)   
+// $('#boat').on('click', moveBoat)
 
-$('#boat').on('click', moveBoat)
-
-$('#previous').on('click', previousEvent)
-$('#next').on('click', nextEvent)
+// $('#previous').on('click', previousEvent)
+// $('#next').on('click', nextEvent)
